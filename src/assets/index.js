01a78ba4e06a3281ff93fba87642c7b352d83209
Susan <suubonnee@gmail.com>
2025-03-17 02:45:26 -0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,101 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import students from './backgrounds/students.jpg';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo-black.png';
+import logotext from './logo/dumera.png';
+
+import shaq from './personal/shaq.png';
+
+import bursary from './icons/bursary.png';
+import interview from './icons/interview.png';
+import relationships from './icons/relationships.png';
+import emotional from './icons/emotional.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import graphql from './tech/graphql.png';
+import postgresql from './tech/postgresql.png';
+import rubyrails from './tech/rubyrails.png';
+
+import coverhunt from './company/coverhunt.png';
+import dcc from './company/dcc.png';
+import kelhel from './company/kelhel.png';
+import microverse from './company/microverse.png';
+
+import komikult from './projects/komikult.jpg';
+import leaderboard from './projects/leaderboard.jpg';
+import math from './projects/math-magicians.jpg';
+import movie from './projects/movie-metro.jpg';
+import nyeusi from './projects/nyeusi.jpg';
+import space from './projects/space-hub.png';
+
+export {
+  bwmap,
+  nairobi,
+  students,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  shaq,
+  bursary,
+  interview,
+  relationships,
+  emotional,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  figma,
+  git,
+  graphql,
+  html,
+  javascript,
+  nodejs,
+  postgresql,
+  rubyrails,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+};
